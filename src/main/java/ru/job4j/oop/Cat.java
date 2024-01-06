64cac07ad4f3bed8725442cd40bde7d1a0a9b9bd
kodarov_salavat <kodarov@gmail.com>
2024-01-07 01:06:24 +0400

    1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String name;
+    private String food;
+
+    public void show() {
+        System.out.printf("There are %s's food %s \n", this.name, this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("black");
+        black.eat("fish");
+        black.show();
+    }
+}
