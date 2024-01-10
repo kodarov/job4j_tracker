9189da963fccd22e88d0b04b554904e493b39180
kodarov_salavat <kodarov@gmail.com>
2024-01-10 04:54:27 +0400

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        int index = 0;
+        Item[] allItems = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                allItems[index] = items[i];
+                index++;
+            }
+        }
+        allItems = Arrays.copyOf(allItems, index);
+        return allItems;
+    }
+
+    public Item[] findByName(String key) {
+        int index = 0;
+        Item[] findItems = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && key.equals(items[i].getName())) {
+                findItems[index] = items[i];
+                index++;
+            }
+        }
+        findItems = Arrays.copyOf(findItems, index);
+        return findItems;
     }
 }
