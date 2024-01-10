3945e3edb3225d43ce8f977c65001a17dcdf66d8
kodarov_salavat <kodarov@gmail.com>
2024-01-10 17:49:03 +0400

    3. Массивы и модели. [#174892 #472070]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -7,20 +7,21 @@ public class Library {
         Book book3 = new Book("Thinking in Java", 1000);
         Book book4 = new Book("Clean code ", 450);
         Book[] books = {book1, book2, book3, book4};
-        for (int i = 0; i < books.length; i++) {
-            System.out.printf("Book - %s, pages - %d \n", books[i].getName(), books[i].getPages());
+        String ln = System.lineSeparator();
+        for (Book book : books) {
+            System.out.printf("Book - %s, pages - %d" + ln, book.getName(), book.getPages());
         }
         System.out.println();
         Book tmp = books[0];
         books[0] = books[3];
         books[3] = tmp;
-        for (int i = 0; i < books.length; i++) {
-            System.out.printf("Book - %s, pages - %d \n", books[i].getName(), books[i].getPages());
+        for (Book book : books) {
+            System.out.printf("Book - %s, pages - %d" + ln, book.getName(), book.getPages());
         }
         System.out.println();
-        for (int i = 0; i < books.length; i++) {
-            if ("Clean code".equals(books[i].getName())) {
-                System.out.println(books[i]);
+        for (Book book : books) {
+            if ("Clean code".equals(book.getName())) {
+                System.out.println(book);
             }
         }
     }
