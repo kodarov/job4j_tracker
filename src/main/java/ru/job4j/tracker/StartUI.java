e38d65a3a43b39da36125f979238e2f1d9c25e2d
kodarov_salavat <kodarov@gmail.com>
2024-01-14 01:51:21 +0400

    3.1. Реализация класса StartUI. Добавление заявки [#500744 #473059]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -68,9 +68,7 @@ public class StartUI {
                 } else {
                     System.out.println("Заявки с именем: " + name + " не найдены.");
                 }
-            } else if (select != 6) {
-                System.out.println("Пользователь выбрал: " + select);
-            } else {
+            } else if (select == 6) {
                 run = false;
             }
         }
