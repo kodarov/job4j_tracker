56b7013f7ecf807e212e3eb39e1d53cb661beecf
kodarov_salavat <kodarov@gmail.com>
2024-01-19 17:19:27 +0400

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #474521]

--- src/main/java/ru/job4j/ex/FactorialRecursion.java
+++ src/main/java/ru/job4j/ex/FactorialRecursion.java
@@ -2,14 +2,14 @@ package ru.job4j.ex;
 
 public class FactorialRecursion {
     public static int calc(int n) {
-        if (n == 1) {
+        if (n == 1 || n == 0) {
             return 1;
         }
         return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
-        int result = calc(3);
+        int result = calc(0);
         System.out.println(result);
     }
 }
