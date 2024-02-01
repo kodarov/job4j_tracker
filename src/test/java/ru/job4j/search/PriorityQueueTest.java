a5453ad3a96f7119e842950e1b0e258c475ede92
kodarov_salavat <kodarov@gmail.com>
2024-02-01 05:39:53 +0400

    5. Очередь с приоритетом на LinkedList [#41670 #477640]

--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PriorityQueueTest {
@@ -22,5 +23,27 @@ public class PriorityQueueTest {
         queue.put(new Task("middle", 5));
         Task result = queue.take();
         assertThat(result.getDescription()).isEqualTo("low");
+    }
+
+    @Test
+    public void whenHigherPriorityThenUrgent1() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low1", 5));
+        queue.put(new Task("low2", 5));
+        queue.put(new Task("middle1", 3));
+        queue.put(new Task("low3", 5));
+        queue.put(new Task("urgent1", 1));
+        queue.put(new Task("middle2", 3));
+        queue.put(new Task("urgent2", 1));
+        Task result = queue.take();
+        assertThat(result.getDescription()).isEqualTo("urgent1");
+    }
+
+    @Test
+    public void whenHigherPriorityThenFirst() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("urgent", 10));
+        Task result = queue.take();
+        assertThat(result.getDescription()).isEqualTo("urgent");
     }
 }
