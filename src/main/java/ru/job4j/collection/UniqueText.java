3766bfcd41146a64f3b480292b4b787b9ad98b4b
kodarov_salavat <kodarov@gmail.com>
2024-02-01 21:38:40 +0400

    2. Идентичные тексты [#212655 #478206]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -17,8 +17,6 @@ public class UniqueText {
                 break;
             }
         }
-        /* for-each origin -> new HashSet. */
-        /* for-each text -> hashSet.contains */
         return result;
     }
 }
