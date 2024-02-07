de1c527cbb2fcf443e192d6c4f3e5c832175ea6b
kodarov_salavat <kodarov@gmail.com>
2024-02-08 02:57:01 +0400

    3. Компаратор для строк [#35008 #479536]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -1,33 +1,15 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int size = Math.min(left.length(), right.length());
-        List<Character> leftList = new ArrayList<>(size);
-        List<Character> rightList = new ArrayList<>(size);
-        for (int i = 0; i < left.length(); i++) {
-            leftList.add(left.charAt(i));
-        }
-        for (int i = 0; i < right.length(); i++) {
-            rightList.add(right.charAt(i));
-        }
-        for (int i = 0; i < size; i++) {
-
-            if (Character.compare(leftList.get(i), rightList.get(i)) < 0) {
-                return -1;
-            }
-            if (Character.compare(leftList.get(i), rightList.get(i)) > 0) {
-                return 1;
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(left.charAt(i), right.charAt(i));
             }
         }
-        if (Integer.compare(leftList.size(), rightList.size()) < 0) {
-            return -1;
-        }
-        return 0;
+        return Integer.compare(left.length(), right.length());
     }
 }
