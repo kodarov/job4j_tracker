0770b4eabccef9203e3174719390259ac3362b0f
kodarov_salavat <kodarov@gmail.com>
2024-02-07 07:24:50 +0400

    PriorityQueue example

---
+++ src/main/java/ru/job4j/queue/PriorityQueueRunner.java
@@ -0,0 +1,44 @@
+package ru.job4j.queue;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueRunner {
+    public static void main(String[] args) {
+        PriorityQueue<String> stringQueue = new PriorityQueue<>();
+        stringQueue.offer("f");
+        stringQueue.offer("a");
+        stringQueue.offer("s");
+        stringQueue.offer("e");
+        printStringElements(stringQueue);
+
+        PriorityQueue<Transaction> transactions = new PriorityQueue<>(new TransactionSortDescByAmount());
+        transactions.offer(new Transaction(1, "number_1", 100));
+        transactions.offer(new Transaction(2, "number_2", 1000));
+        transactions.offer(new Transaction(3, "number_3", 85));
+        transactions.offer(new Transaction(4, "number_4", 550));
+        printTransactionElements(transactions);
+
+        PriorityQueue<String> stringQueue2 = new PriorityQueue<>();
+        stringQueue2.offer("f");
+        stringQueue2.offer("a");
+        stringQueue2.offer("s");
+        stringQueue2.offer("e");
+        System.out.println("Current element: " + stringQueue2.peek());
+        stringQueue2.poll();
+        System.out.println("Current element: " + stringQueue2.peek());
+        stringQueue2.poll();
+        System.out.println("Current element: " + stringQueue2.peek());
+    }
+
+    private static void printStringElements(PriorityQueue<String> queue) {
+        while (!queue.isEmpty()) {
+            System.out.println("Current element: " + queue.poll());
+        }
+    }
+
+    private static void printTransactionElements(PriorityQueue<Transaction> queue) {
+        while (!queue.isEmpty()) {
+            System.out.println("Current element: " + queue.poll());
+        }
+    }
+}
