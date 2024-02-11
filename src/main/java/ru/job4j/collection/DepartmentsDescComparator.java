0976b705fef8c17cd337c508233f5422d80db262
kodarov_salavat <kodarov@gmail.com>
2024-02-11 21:43:13 +0400

    3. Отсортировать департаменты [#34352 #480415]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -1,24 +1,15 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        List<String> leftList = new ArrayList<>(Arrays.asList(left.split("/")));
-        List<String> rightList = new ArrayList<>(Arrays.asList(right.split("/")));
-        for (int i = 0; i < Math.min(rightList.size(), leftList.size()); i++) {
-            if (!leftList.get(0).equals(rightList.get(0))) {
-                return rightList.get(0).compareTo(leftList.get(0));
-            }
-            if (leftList.get(i).equals(rightList.get(i))) {
-                continue;
-            }
-            return leftList.get(i).compareTo(rightList.get(i));
+        String[] leftArray = left.split("/");
+        String[] rightArray = right.split("/");
+        if (leftArray[0].compareTo(rightArray[0]) != 0) {
+            return rightArray[0].compareTo(leftArray[0]);
         }
-        return Integer.compare(left.length(), right.length());
+        return left.compareTo(right);
     }
 }
