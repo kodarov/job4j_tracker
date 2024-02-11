b0722fb6dc827fda0fc3363fc3900a4e4c8bedd6
kodarov_salavat <kodarov@gmail.com>
2024-02-11 04:56:42 +0400

    2.1. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("eeeee", "a", "ccc", "dddd", "bb");
+        Comparator<String> comparator = (left, right) -> {
+            System.out.format("left.length: %s, right.length: %s", left.length(), right.length()
+                    + System.lineSeparator());
+            return Integer.compare(right.length(), left.length());
+        };
+        strings.sort(comparator);
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
