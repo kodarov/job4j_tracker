31008a2c2bc032d6eb306b8b6e2b3e1d6d5c13fc
kodarov_salavat <kodarov@gmail.com>
2024-02-16 01:54:26 +0400

    15. Агрегатный метод reduce() [#504929 #481545]

---
+++ src/main/java/ru/job4j/stream/MatchExample.java
@@ -0,0 +1,33 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MatchExample {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("Один", "Два", "Три", "Четыре", "Пять");
+        boolean result = strings
+                .stream()
+                .noneMatch("Шесть"::contains);
+        System.out.println(result);
+
+        anyM();
+        allM();
+    }
+
+    private static void anyM() {
+        List<String> strings = Arrays.asList("Один", "Два", "Три", "Четыре", "Пять");
+        boolean result = strings
+                .stream()
+                .anyMatch(element -> element.endsWith("ь"));
+        System.out.println(result);
+    }
+
+    private static void allM() {
+        List<String> strings = Arrays.asList("Трижды", "Три", "Триста", "Три тысячи");
+        boolean result = strings
+                .stream()
+                .allMatch(element -> element.startsWith("Три"));
+        System.out.println(result);
+    }
+}
