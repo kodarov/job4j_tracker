8e61c52781b46e5c6c81bd64ce90e0979a0997db
kodarov_salavat <kodarov@gmail.com>
2024-02-17 05:34:13 +0400

    2. Local-Variable Type Inference [#70626]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -2,24 +2,22 @@ package ru.job4j.search;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        List<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 
     @Test
     public void whenFindByNameThenEmpty() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        List<Person> persons = phones.find("Java");
+        var persons = phones.find("Java");
         assertThat(persons).isEmpty();
     }
 }
--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -7,27 +7,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PriorityQueueTest {
     @Test
     public void whenHigherPrioritySecond() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDescription()).isEqualTo("urgent");
     }
 
     @Test
     public void whenHigherPriorityEquals() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 5));
         queue.put(new Task("middle", 5));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDescription()).isEqualTo("low");
     }
 
     @Test
     public void whenHigherPriorityThenUrgent1() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low1", 5));
         queue.put(new Task("low2", 5));
         queue.put(new Task("middle1", 3));
@@ -35,15 +35,15 @@ public class PriorityQueueTest {
         queue.put(new Task("urgent1", 1));
         queue.put(new Task("middle2", 3));
         queue.put(new Task("urgent2", 1));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDescription()).isEqualTo("urgent1");
     }
 
     @Test
     public void whenHigherPriorityThenFirst() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("urgent", 10));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDescription()).isEqualTo("urgent");
     }
 }
